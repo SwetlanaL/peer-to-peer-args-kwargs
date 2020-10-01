56305095c4e6581af2cdc74305895f7d4b428efa
Svetlana Leovochkina <sleovochkina@gmail.com>
2020-10-01 09:50:06 +0300

    Slight correction

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ def get_absolute_url(required, *args, **kwargs):
     :param *args: позиционные аргументы
     :param *kwargs: именованные аргументы
     """
-    url_string = ''
+    url = required
     # Формирование строки до '?' включительно, использование метода join с итерированием элементов args
-    url_string += (required + '/' + '/'.join(i for i in args) + '?')
+    url += '/' + '/'.join(i for i in args) + '?'
     # Формирование строки c присоединением именнованых аргументов 
     for k, v in kwargs.items():
-        url_string += (k + '=' + v + '&')
+        url += (k + '=' + v + '&')
     #Возвращение строки без последнего '&'
-    return(url_string[:-1])   
+    return(url[:-1])   
     
 print(get_absolute_url('www.yandex.ru', 'posts', 'news', id='24', author='admin'))
 print(get_absolute_url('www.google.com', 'images', id='24', category='auto', color='red', size='small'))
+
+
+
+    
